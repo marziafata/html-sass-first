821a6a54a8ea7ca31289d5092479178b451bebf0
Marzia Fatarella <marziafatarella@gmail.com>
2020-06-06 17:09:07 +0200

    compilazione sass

--- public/app.js
+++ public/app.js
@@ -115,8 +115,8 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/marziafata/Desktop/Boolean/Playground/prima-app/src/app.js */"./src/app.js");
-module.exports = __webpack_require__(/*! /Users/marziafata/Desktop/Boolean/Playground/prima-app/src/app.scss */"./src/app.scss");
+__webpack_require__(/*! /Users/marziafata/Desktop/Boolean/Playground/37-sass/prima-app/src/app.js */"./src/app.js");
+module.exports = __webpack_require__(/*! /Users/marziafata/Desktop/Boolean/Playground/37-sass/prima-app/src/app.scss */"./src/app.scss");
 
 
 /***/ })
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('src/app.js', 'public/').sass('src/app.scss', 'public/');
+mix.js('src/app.js', 'public/').sass('src/app.scss', 'public/app.css');
 
 // Full API
 // mix.js(src, output);
